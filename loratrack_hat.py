6c7dd14d8fb3690da5f14de731e167f851a542db
DomRicho <DomRicho15@outlook.com>
2025-09-10 17:52:30 +1000

    add node nav info

--- loratrack_hat.py
+++ loratrack_hat.py
@@ -6,11 +6,12 @@ class Node():
         self.timestamp = 0
         self.ticks = 0
         self.pos = (0, 0)
+        self.nav = (0, 0, 0)
         self.fix_status = 0
         self.poshold = 0
 
     def add_lora_info(self, info):
         pass
 
-    def add_nav(self, nav):
-        pass
+    def add_nav(self, lat, lon, alt):
+        self.nav = (lat, lon, alt)
